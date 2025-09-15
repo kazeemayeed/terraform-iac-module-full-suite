f4a84745763c6fcdeb80ff2fdb673c7db9209260
kazeem mohammed <224498298+kazeemayeed@users.noreply.github.com>
2025-09-15 01:13:05 -0500

    Update variables.tf

--- modules/module-full-suite/variables.tf
+++ modules/module-full-suite/variables.tf
@@ -8,6 +8,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_subnet_cidrs" {
