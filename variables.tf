d043db289a75dd6dda0b29c87bf1cd8b9d273e86
kazeem mohammed <224498298+kazeemayeed@users.noreply.github.com>
2025-09-19 00:29:51 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair name for EC2 instances"
+  type        = string
+  # You need to create this key pair in your AWS console first
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "terraform-full-suite"
+}
