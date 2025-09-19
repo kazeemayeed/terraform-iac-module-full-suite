47a708b5aa2a9782d4f7068f0abef4dfcf3875c1
kazeem mohammed <224498298+kazeemayeed@users.noreply.github.com>
2025-09-19 00:39:14 -0500

    Update outputs.tf

--- modules/module-full-suite/outputs.tf
+++ modules/module-full-suite/outputs.tf
@@ -1,38 +1,42 @@
-# modules/module-full-suite/outputs.tf
-
-# Network Module Outputs
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = module.network.vpc_id
+  value       = aws_vpc.main.id
 }
 
-output "load_balancer_dns" {
-  description = "DNS name of the load balancer"
-  value       = module.compute.load_balancer_dns
-}
-
-output "website_url" {
-  description = "URL to access the website"
-  value       = "http://${module.compute.load_balancer_dns}"
-}
-
-# Additional useful outputs
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = module.network.public_subnet_ids
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
   description = "IDs of the private subnets"
-  value       = module.network.private_subnet_ids
+  value       = aws_subnet.private[*].id
 }
 
-output "auto_scaling_group_arn" {
-  description = "ARN of the Auto Scaling Group"
-  value       = try(module.compute.auto_scaling_group_arn, null)
+# Load balancer outputs - reference the actual resources in main.tf
+output "load_balancer_dns" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
 }
 
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = try(module.compute.security_group_id, null)
+output "website_url" {
+  description = "URL to access the website"
+  value       = "http://${aws_lb.main.dns_name}"
+}
+
+# Security group outputs
+output "alb_security_group_id" {
+  description = "ID of the ALB security group"
+  value       = aws_security_group.alb.id
+}
+
+output "ec2_security_group_id" {
+  description = "ID of the EC2 security group"
+  value       = aws_security_group.ec2.id
+}
+
+# Target group output
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.main.arn
 }
